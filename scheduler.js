ff0f3f53c71c8c4270eb209096aa0f2211157bb0
Pedro Fonseca <pedro@MacBook-Pro-de-Pedro.local>
2022-11-17 13:19:41 -0300

    Alterando pra 10s o cron

--- scheduler.js
+++ scheduler.js
@@ -7,7 +7,7 @@ const moment = require('moment');
 const schedulerFactory = function() {
   return {
     start: function() {
-      new CronJob('00 * * * * *', function() {
+      new CronJob('*/10 * * * * *', function() {
         console.log('Running Send Notifications Worker for ' +
           moment().format());
         emailDeliveryJob.run();
